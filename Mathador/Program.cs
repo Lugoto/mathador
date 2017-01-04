9c7c5bfb3c460d9c30a5638caa9915a61a8a043d
Lugoto <pozzerlucas@gmail.com>
2017-01-04 15:48:13 +0100

    générateur sans fonction

--- Mathador/Program.cs
+++ Mathador/Program.cs
@@ -10,14 +10,127 @@ namespace Mathador
     {
         static void Main(string[] args)
         {
-          
-            Random rnd = new Random();
-
-            Console.WriteLine("\n20 random integers from 0 to 100:");
-            for (int ctr = 1; ctr <= 3; ctr++)
+            int number2 = 0;
+            
+            while (number2 < 1 || number2 > 101)
             {
-                Console.Write("{0,6}", rnd.Next(1, 12));
-                if (ctr % 5 == 0) Console.WriteLine();
+                Random rnd = new Random();
+                Random r = new Random();
+                int add = 0;
+                int sous = 0;
+                int mult = 0;
+                int div = 0;
+
+                for (int ctr = 1; ctr <= 3; ctr++)
+                {
+                    
+                    int number = rnd.Next(1, 12);
+                    Console.WriteLine("random");
+                    Console.WriteLine(number);
+                    if (ctr <= 1)
+                    {
+                        number2 = number;
+                        Console.WriteLine("premier nombre");
+                        Console.WriteLine(number2);
+                    }
+                    if (ctr > 1)
+                    {
+                        bool verif = false;
+                        while (verif == false)
+                        {
+                            int num = r.Next(1, 5);
+                            if (num == 1 && add == 0)
+                            {
+                                number2 = number2 + number;
+                                Console.WriteLine("addition");
+                                Console.WriteLine(number2);
+                                add = 1;
+                                verif = true;
+                            }
+                            else if (num == 2 && sous == 0)
+                            {
+                                number2 = number2 - number;
+                                Console.WriteLine("soustraction");
+                                Console.WriteLine(number2);
+                                sous = 1;
+                                verif = true;
+                            }
+                            else if (num == 3 && mult == 0)
+                            {
+                                number2 = number2 * number;
+                                Console.WriteLine("multiplication");
+                                Console.WriteLine(number2);
+                                mult = 1;
+                                verif = true;
+                            }
+                            else if (num == 4 && div == 0)
+                            {
+                                number2 = number2 / number;
+                                Console.WriteLine("division");
+                                Console.WriteLine(number2);
+                                div = 1;
+                                verif = true;
+                            }
+                        }
+                    }
+                }
+                for (int ctr = 2; ctr <= 3; ctr++)
+                {
+                    while (add == 0 || sous == 0 || mult == 0 || div == 0)
+                    {
+                    int number = rnd.Next(1, 20);
+                    Console.WriteLine("random");
+                    Console.WriteLine(number);
+                    if (ctr <= 1)
+                    {
+                        number2 = number;
+                        Console.WriteLine("affiche");
+                        Console.WriteLine(number2);
+                    }
+                        if (ctr > 1)
+                        {
+                            bool verif = false;
+                            while (verif == false)
+                            {
+                                int num = r.Next(1, 5);
+                                if (num == 1 && add == 0)
+                                {
+                                    number2 = number2 + number;
+                                    Console.WriteLine("addition");
+                                    Console.WriteLine(number2);
+                                    add = 1;
+                                    verif = true;
+                                }
+                                else if (num == 2 && sous == 0)
+                                {
+                                    number2 = number2 - number;
+                                    Console.WriteLine("soustraction");
+                                    Console.WriteLine(number2);
+                                    sous = 1;
+                                    verif = true;
+                                }
+                                else if (num == 3 && mult == 0)
+                                {
+                                    number2 = number2 * number;
+                                    Console.WriteLine("multiplication");
+                                    Console.WriteLine(number2);
+                                    mult = 1;
+                                    verif = true;
+                                }
+                                else if (num == 4 && div == 0)
+                                {
+                                    number2 = number2 / number;
+                                    Console.WriteLine("division");
+                                    Console.WriteLine(number2);
+                                    div = 1;
+                                    verif = true;
+                                }
+                            }
+                        }
+                }
+                }
+                Console.WriteLine("--------------------FINALUS--------------------");
+                Console.WriteLine(number2);
             }
             Console.ReadLine();
         }
